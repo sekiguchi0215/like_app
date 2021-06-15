33caf5943daec06990b3bcaf0360e806ac9a7c6a
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-15 22:59:56 +0900

    「いいね！」しているかどうかを判定するメソッドを作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_user, through: :likes, source: :user
   validates :content, presence: true
+
+  def liked_by?(user)
+    likes.exists?(user_id: user.id)
+  end
 end
