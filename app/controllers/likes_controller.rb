ff3067b81d35c0b2222d71e5531eff6e05c777b6
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-16 02:16:07 +0900

    「いいね！」数の表示を実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,11 @@
 class LikesController < ApplicationController
   def create
-    @post = Post.find(params[:post_id])
     current_user.likes.create!(post_id: params[:post_id])
+    @post = Post.find(params[:post_id])
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
     current_user.likes.find_by(post_id: params[:post_id]).destroy!
+    @post = Post.find(params[:post_id])
   end
 end
