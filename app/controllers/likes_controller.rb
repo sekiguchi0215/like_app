a19ba00d00965280f0fb319ffe4293ae02fe8b62
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-16 01:56:10 +0900

    「いいね！」機能を 非同期処理 に変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,11 @@
 class LikesController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
     current_user.likes.create!(post_id: params[:post_id])
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     current_user.likes.find_by(post_id: params[:post_id]).destroy!
-    redirect_back(fallback_location: root_path)
   end
 end
