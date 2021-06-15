a97bf7c87d79fff7df70442caa9da3eba01b8a0f
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-15 22:57:38 +0900

    多対多の関連付け

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
+  has_many :liked_user, through: :likes, source: :user
   validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
 end
