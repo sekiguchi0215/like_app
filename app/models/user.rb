6a4502c3eebea31341386d39bd11a509b57d17c2
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-16 16:01:27 +0900

    ゲストログイン機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
+
+  def self.guest
+    find_or_create_by!(email: "guest@example.com") do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
