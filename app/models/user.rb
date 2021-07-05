2209ef9eb44814c9e3eb54157df3bb8849021f42
sekiguchi0215 <dsphere5296@gmail.com>
2021-07-05 14:53:17 +0900

    relationships モデルと user モデルにアソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
 
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "follow_id"
+  has_many :followers, through: :reverse_of_relationships, source: :user
+
   def self.guest
     find_or_create_by!(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
